7d8b7a91c35eac13b36195287ee80a90b0ec235f
Aditya Pratap <adityapratap7344@gmail.com>
2019-06-05 03:23:16 +0530

    Updates

---
+++ precache-manifest.c49bb93436fe492725f909ba74c61d40.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724244a37f4d38432a2a914bc4f4da61",
+    "url": "/Asilverse-Landing-Page/index.html"
+  },
+  {
+    "revision": "4225d4e3a93b1d533a7c",
+    "url": "/Asilverse-Landing-Page/static/css/2.7b19e11b.chunk.css"
+  },
+  {
+    "revision": "2bd637c68bed2ff68d3c",
+    "url": "/Asilverse-Landing-Page/static/css/main.2dd701d8.chunk.css"
+  },
+  {
+    "revision": "4225d4e3a93b1d533a7c",
+    "url": "/Asilverse-Landing-Page/static/js/2.ed73a001.chunk.js"
+  },
+  {
+    "revision": "2bd637c68bed2ff68d3c",
+    "url": "/Asilverse-Landing-Page/static/js/main.3d964a33.chunk.js"
+  },
+  {
+    "revision": "e562c433f2401b80bef7",
+    "url": "/Asilverse-Landing-Page/static/js/runtime~main.d8460535.js"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "/Asilverse-Landing-Page/static/media/AirbnbCereal-Light.1f9c7d32.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/Asilverse-Landing-Page/static/media/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "eeeb6c2e2dfe8fc1b981b56a70f9a181",
+    "url": "/Asilverse-Landing-Page/static/media/Group 10.eeeb6c2e.svg"
+  },
+  {
+    "revision": "06489a1f50fe07b2616a0dda8ff77db9",
+    "url": "/Asilverse-Landing-Page/static/media/Group 18.06489a1f.svg"
+  },
+  {
+    "revision": "b015067931618c4af8a689fcedf52ab6",
+    "url": "/Asilverse-Landing-Page/static/media/Group 9.b0150679.svg"
+  },
+  {
+    "revision": "d0707093e0c323a410e438ef607811d4",
+    "url": "/Asilverse-Landing-Page/static/media/Path.d0707093.svg"
+  },
+  {
+    "revision": "707285c3bb956f30889d8c5f8b502bfc",
+    "url": "/Asilverse-Landing-Page/static/media/Pebble Purple.707285c3.svg"
+  },
+  {
+    "revision": "50dd3a7a0572df16259a52c660983ec0",
+    "url": "/Asilverse-Landing-Page/static/media/Wave Arrow.50dd3a7a.svg"
+  },
+  {
+    "revision": "119e778da21adf2f99d36a4f0f4cda8b",
+    "url": "/Asilverse-Landing-Page/static/media/guarantee.119e778d.svg"
+  },
+  {
+    "revision": "ce033e7feb74744d3b9fde1b7bcf25c5",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-design.ce033e7f.svg"
+  },
+  {
+    "revision": "ecbedd291ba84d67424e04948d767ba8",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-facebook.ecbedd29.svg"
+  },
+  {
+    "revision": "6bcd3287145558ddddb371504450ff18",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-google_logo.6bcd3287.svg"
+  },
+  {
+    "revision": "7eef2110a70f02ba9c79a862050454a8",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-instagram_new.7eef2110.svg"
+  },
+  {
+    "revision": "989bb4db438cf45af44c197def48adc3",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-linkedin.989bb4db.svg"
+  },
+  {
+    "revision": "896dba58941d93c6365a77491784c417",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-neutral_trading.896dba58.svg"
+  },
+  {
+    "revision": "0df2920fc714597012357f5d748c868d",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-search_bar.0df2920f.svg"
+  },
+  {
+    "revision": "f0573a9fef8effeb83c234a57b2cdfd7",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-terms_and_conditions.f0573a9f.svg"
+  },
+  {
+    "revision": "0c1c70f55c78f6f5aa9913be87193a2d",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-to_do.0c1c70f5.svg"
+  },
+  {
+    "revision": "cad43c018cf081e662c5bf901732886d",
+    "url": "/Asilverse-Landing-Page/static/media/icons8-twitter.cad43c01.svg"
+  },
+  {
+    "revision": "33c6a499c5a9eb2e2f789ae90f307e61",
+    "url": "/Asilverse-Landing-Page/static/media/idea.33c6a499.svg"
+  },
+  {
+    "revision": "c6add8de7d20e12fb839a615fad0fa96",
+    "url": "/Asilverse-Landing-Page/static/media/market-analysis.c6add8de.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Asilverse-Landing-Page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Asilverse-Landing-Page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Asilverse-Landing-Page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Asilverse-Landing-Page/static/media/slick.f97e3bbf.svg"
+  }
+]);
